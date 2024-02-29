4013304d0444035075615755291e6fefe52de75a
jordi <jordielgueta@gmail.com>
2024-02-29 10:48:59 +0100

    icons updated in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,15 +2,16 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import dashboardIcon from "../../public/icons/dashboard_icon.svg";
-import roadmapIcon from "../../public/icons/roadmap_icon.svg";
-import materialsIcon from "../../public/icons/materials_icon.svg";
-import communityIcon from "../../public/icons/community_icon.svg";
-import contactIcon from "../../public/icons/contact_icon.svg";
+import { useTheme } from "next-themes";
+import DashboardIcon from "./icons/DashboardIcon";
+import RoadmapIcon from "./icons/RoadmapIcon";
+import MaterialsIcon from "./icons/MaterialsIcon";
+import CommunityIcon from "./icons/CommunityIcon";
+import ContactIcon from "./icons/ContactIcon";
 
 const Sidebar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { theme } = useTheme();
   const handleNav = () => {
     setMenuOpen(!menuOpen);
   };
@@ -27,49 +28,59 @@ const Sidebar: React.FC = () => {
         </Link>
         <div>
           <ul className="mt-10 flex flex-col items-start h-full w-full">
-            <li className="text-xl transform transition duration-350 hover:scale-115">
-              <div className="flex items-center">
-                <Image
-                  src={dashboardIcon}
-                  alt="dashboard"
-                  width={25}
-                  height={25}
-                />
-                <Link href="/dashboard">Home</Link>
+            <li className="text-xl transform transition duration-350 hover:scale-115 mt-8">
+              <div className="flex items-center gap-4">
+                <DashboardIcon color={theme === "dark" ? "white" : "#818181"} />
+                <Link
+                  href="/dashboard"
+                  className="text-[#818181] dark:text-white"
+                >
+                  Home
+                </Link>
               </div>
             </li>
-            <li className="text-xl transform transition duration-350 hover:scale-115">
-              <div className="flex items-center">
-                <Image src={roadmapIcon} alt="roadmap" width={25} height={25} />
-                <Link href="/roadmap">Roadmap</Link>
+            <li className="text-xl transform transition duration-350 hover:scale-115 mt-8">
+              <div className="flex items-center gap-4">
+                <RoadmapIcon color={theme === "dark" ? "white" : "#818181"} />
+                <Link
+                  href="/roadmap"
+                  className="text-[#818181] dark:text-white"
+                >
+                  Roadmap
+                </Link>
               </div>
             </li>
-            <li className="text-xl transform transition duration-350 hover:scale-115">
-              <div className="flex items-center">
-                <Image
-                  src={materialsIcon}
-                  alt="materials"
-                  width={25}
-                  height={25}
-                />
-                <Link href="/materials">Materials</Link>
+            <li className="text-xl transform transition duration-350 hover:scale-115 mt-8">
+              <div className="flex items-center gap-4">
+                <MaterialsIcon color={theme === "dark" ? "white" : "#818181"} />
+                <Link
+                  href="/materials"
+                  className="text-[#818181] dark:text-white"
+                >
+                  Materials
+                </Link>
               </div>
             </li>
-            <li className="text-xl transform transition duration-350 hover:scale-115">
-              <div className="flex items-center">
-                <Image
-                  src={communityIcon}
-                  alt="community"
-                  width={25}
-                  height={25}
-                />
-                <Link href="/community">Community</Link>
+            <li className="text-xl transform transition duration-350 hover:scale-115 mt-8">
+              <div className="flex items-center gap-4">
+                <CommunityIcon color={theme === "dark" ? "white" : "#818181"} />
+                <Link
+                  href="/community"
+                  className="text-[#818181] dark:text-white"
+                >
+                  Community
+                </Link>
               </div>
             </li>
-            <li className="text-xl transform transition duration-350 hover:scale-115">
-              <div className="flex items-center">
-                <Image src={contactIcon} alt="contact" width={25} height={25} />
-                <Link href="/contact">Contact</Link>
+            <li className="text-xl transform transition duration-350 hover:scale-115 mt-8">
+              <div className="flex items-center gap-4">
+                <ContactIcon color={theme === "dark" ? "white" : "#818181"} />
+                <Link
+                  href="/contact"
+                  className="text-[#818181] dark:text-white"
+                >
+                  Contact
+                </Link>
               </div>
             </li>
           </ul>
