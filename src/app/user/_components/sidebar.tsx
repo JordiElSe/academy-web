93e4bae3425000b30217a5018d10667b20fa7f77
jordi <jordielgueta@gmail.com>
2024-05-14 15:08:16 +0200

    fixes in the sidebar

--- src/app/user/_components/sidebar.tsx
+++ src/app/user/_components/sidebar.tsx
@@ -50,107 +50,78 @@ const Sidebar: FC<Props> = ({ handleSidebar, isNarrow }) => {
         }`}
       >
         <nav className="bg-slate-300 dark:bg-[rgb(31,55,98,0.93)] relative shadow-md h-full w-full">
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full h-full">
             <Link className="mt-10" href="/">
               <div> Logo </div>
             </Link>
             <Separator className="h-1" />
             <div>
-              <ul className="mt-10 flex flex-col h-full w-full">
-                {navItemsTop.map((item) => (
-                  <li
-                    className={`pl-8 text-base transform transition duration-350 hover:scale-115 mt-8 ${
-                      pathname === item.href
-                        ? "bg-gradient-to-r from-purple-500 to-white"
-                        : ""
-                    }`}
-                    key={item.label}
-                  >
-                    <div className="flex items-center gap-4">
-                      <item.icon
-                        color={theme === "dark" ? "white" : "#434343"}
-                      />
-                      {!isNarrow && (
-                        <Link
-                          href={item.href}
-                          className="text-[#434343] dark:text-white"
-                        >
-                          {item.label}
-                        </Link>
-                      )}
-                    </div>
-                  </li>
+              <ul className="mt-10 flex flex-col h-full w-full gap-4">
+                {navItemsTop.map((item, index) => (
+                  <Link href={item.href} key={index} className="w-full">
+                    <li
+                      className={`flex items-center pl-8 h-[3rem] w-full text-base transform transition duration-350 hover:scale-115 ${
+                        pathname === item.href
+                          ? "bg-gradient-to-r from-slate-400 to-slate-300 dark:from-slate-500 dark:to-slate-400"
+                          : ""
+                      }`}
+                    >
+                      <div className="flex items-center gap-4 w-full">
+                        <item.icon
+                          color={theme === "dark" ? "white" : "#434343"}
+                        />
+                        {!isNarrow && (
+                          <div className="text-[#434343] dark:text-white">
+                            {item.label}
+                          </div>
+                        )}
+                      </div>
+                    </li>
+                  </Link>
                 ))}
               </ul>
             </div>
-            <div>
-              <ul className="mt-10 flex flex-col h-full w-full">
-                {navItemsBottom.map((item) => (
-                  <li
-                    className="text-base transform transition duration-350 hover:scale-115 mt-8"
-                    key={item.label}
-                  >
-                    <div className="flex items-center gap-4">
-                      <item.icon
-                        color={theme === "dark" ? "white" : "#434343"}
-                      />
-                      {!isNarrow && (
-                        <Link
-                          href={item.href}
-                          className="text-[#434343] dark:text-white"
-                        >
-                          {item.label}
-                        </Link>
-                      )}
-                    </div>
-                  </li>
+            <div className="mt-auto mb-4">
+              <ul className="flex flex-col h-full w-full gap-4">
+                {navItemsBottom.map((item, index) => (
+                  <Link href={item.href} key={index} className="w-full">
+                    <li
+                      className={`flex items-center pl-8 h-[3rem] w-full text-base transform transition duration-350 hover:scale-115 ${
+                        pathname === item.href
+                          ? "bg-gradient-to-r from-slate-400 to-slate-300 dark:from-slate-500 dark:to-slate-400"
+                          : ""
+                      }`}
+                      key={item.label}
+                    >
+                      <div className="flex items-center gap-4 w-full">
+                        <item.icon
+                          color={theme === "dark" ? "white" : "#434343"}
+                        />
+                        {!isNarrow && (
+                          <div className="text-[#434343] dark:text-white">
+                            {item.label}
+                          </div>
+                        )}
+                      </div>
+                    </li>
+                  </Link>
                 ))}
               </ul>
             </div>
-          </div>
-          <div
-            onClick={handleSidebar}
-            className="absolute bottom-0 right-0 cursor-pointer mr-5 mb-5"
-          >
-            <DoubleArrowLeftIcon
-              className={`transition transform duration-300 ${
-                isNarrow ? "rotate-180" : ""
-              }`}
-              color={theme === "dark" ? "white" : "#818181"}
-            />
+            <div
+              onClick={handleSidebar}
+              className="ml-auto cursor-pointer mr-5 mb-5"
+            >
+              <DoubleArrowLeftIcon
+                className={`transition transform duration-300 ${
+                  isNarrow ? "rotate-180" : ""
+                }`}
+                color={theme === "dark" ? "white" : "#818181"}
+              />
+            </div>
           </div>
         </nav>
       </aside>
-      {/* <div>
-        <div
-          className={`fixed top-20 w-full md:hidden h-full bg-slate-300 dark:bg-[rgb(31,55,98,0.93)] p-10 ease-in duration-500 ${
-            showSSsidebar ? "left-0" : "left-[-100%]"
-          }`}
-        >
-          <div className="flex flex-col items-center">
-            <ul>
-              {navItemsBottom.map((item) => (
-                <li
-                  className="p-3 hover:border-b cursor-pointer text-xl"
-                  key={item.label}
-                >
-                  <div className="flex items-center gap-4">
-                    <item.icon color={theme === "dark" ? "white" : "#818181"} />
-                    {!isNarrow && (
-                      <Link
-                        href={item.href}
-                        className="text-[#818181] dark:text-white"
-                      >
-                        {item.label}
-                      </Link>
-                    )}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
