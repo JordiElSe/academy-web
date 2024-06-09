226fd1f682aff78e69c77cde196f067189403303
jordi <jordielgueta@gmail.com>
2024-06-09 23:45:14 +0200

    added first stop on the roadmap

--- src/app/(public)/_components/path.tsx
+++ src/app/(public)/_components/path.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
+import Stop from "./stop";
 import { useScroll, useMotionValueEvent } from "framer-motion";
 import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
 
 const Path = () => {
   const { theme } = useTheme();
@@ -24,12 +26,12 @@ const Path = () => {
     }
   });
 
-  let strokeWidth = 15;
+  let strokeWidth = 5;
   let svgWidth = 1458.5;
   let svgHeight = 1000;
   let repetitions = 2;
 
-  let startPoint = [(svgWidth + strokeWidth) / 2, strokeWidth / 2];
+  let startPoint = [(svgWidth + strokeWidth) / 2, strokeWidth / 2 + 30];
   let endPoint1 = [startPoint[0], startPoint[1] + svgHeight / 2];
   let endPoint2 = [startPoint[0], startPoint[1] + svgHeight];
   let controlPoint1 = [startPoint[0] - svgWidth / 2, startPoint[1]];
@@ -55,6 +57,45 @@ const Path = () => {
       controlPoint4[1] + yOffset
     } ${endPoint2[0]} ${endPoint2[1] + yOffset}`;
   }
+
+  const baseHeight = 13;
+  const baseWidth = 100;
+  const baseDepth = 35;
+  const buttonHeight = baseHeight;
+  const [buttonClickedHeight, setButtonClickedHeight] = useState(0);
+  const buttonWidth = baseWidth * 0.58;
+  const buttonDepth = baseDepth / 2;
+  // const svgHeight = baseHeight + buttonHeight + baseDepth / 2 + buttonDepth;
+
+  const centerBaseEllipse = [startPoint[0], startPoint[1] - 5];
+  const centerButtonEllipse = [startPoint[0], startPoint[1] - 5 - buttonHeight];
+  const buttonTopLeft = [
+    centerButtonEllipse[0] - buttonWidth / 2,
+    centerButtonEllipse[1],
+  ];
+  const buttonTopRight = [
+    buttonTopLeft[0] + buttonWidth,
+    centerButtonEllipse[1],
+  ];
+  const buttonBottomRight = [
+    buttonTopRight[0],
+    centerButtonEllipse[1] + buttonHeight,
+  ];
+  const buttonBottomLeft = [buttonTopLeft[0], buttonBottomRight[1]];
+  const baseTopLeft = [
+    centerBaseEllipse[0] - baseWidth / 2,
+    centerBaseEllipse[1],
+  ];
+  const baseTopRight = [
+    centerBaseEllipse[0] + baseWidth / 2,
+    centerBaseEllipse[1],
+  ];
+  const baseBottomRight = [baseTopRight[0], centerBaseEllipse[1] + baseHeight];
+  const baseBottomLeft = [baseTopLeft[0], baseBottomRight[1]];
+
+  const handleClick = () => {
+    setButtonClickedHeight((prev) => (prev === 0 ? buttonHeight * 0.7 : 0));
+  };
 
   return (
     <svg
@@ -92,6 +133,7 @@ const Path = () => {
           />
         </linearGradient>
       </defs>
+
       <path
         ref={pathRef}
         d={d}
@@ -106,6 +148,126 @@ const Path = () => {
         stroke="url(#roadmapGradient)"
         strokeWidth={strokeWidth}
         fill="none"
+      />
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight - 5}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+          times: [0, 1.1, 2],
+          repeatDelay: 0.5,
+        }}
+      />
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight - 5}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+          delay: 0.25,
+          times: [0, 1.1, 2],
+          repeatDelay: 0.5,
+        }}
+      />
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight - 5}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+          delay: 0.5,
+          times: [0, 1.1, 2],
+          repeatDelay: 0.5,
+        }}
+      />
+      <path
+        onClick={handleClick}
+        d={`M${baseTopLeft[0]} ${baseTopLeft[1]} L${baseBottomLeft[0]} ${
+          baseBottomLeft[1]
+        } A ${baseWidth / 2} ${baseDepth / 2} 0 1 0 ${baseBottomRight[0]} ${
+          baseBottomRight[1]
+        } L${baseTopRight[0]} ${baseTopRight[1]}`}
+        fill="#4F4F4F"
+        stroke="black"
+      />
+      <ellipse
+        onClick={handleClick}
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1]}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="#838383"
+        stroke="black"
+      />
+
+      <motion.path
+        onClick={handleClick}
+        d={`M${buttonTopLeft[0]} ${buttonTopLeft[1]} L${buttonBottomLeft[0]} ${
+          buttonBottomLeft[1]
+        } A ${buttonWidth / 2} ${buttonDepth / 2} 0 1 0 ${
+          buttonBottomRight[0]
+        } ${buttonBottomRight[1]} L${buttonTopRight[0]} ${buttonTopRight[1]}`}
+        animate={{
+          d: `M${buttonTopLeft[0]} ${buttonTopLeft[1] + buttonClickedHeight} L${
+            buttonBottomLeft[0]
+          } ${buttonBottomLeft[1]} A ${buttonWidth / 2} ${
+            buttonDepth / 2
+          } 0 1 0 ${buttonBottomRight[0]} ${buttonBottomRight[1]} L${
+            buttonTopRight[0]
+          } ${buttonTopRight[1] + buttonClickedHeight}`,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          damping: 15,
+        }}
+        fill="#4F4F4F"
+        stroke="black"
+      />
+      <motion.ellipse
+        onClick={handleClick}
+        cx={centerButtonEllipse[0]}
+        cy={centerButtonEllipse[1]}
+        rx={buttonWidth / 2}
+        ry={buttonDepth / 2}
+        animate={{
+          cy: centerButtonEllipse[1] + buttonClickedHeight,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          damping: 20,
+        }}
+        fill="#838383"
+        stroke="black"
       />
     </svg>
   );
--- src/app/(public)/_components/stop.tsx
+++ src/app/(public)/_components/stop.tsx
@@ -1,8 +1,183 @@
-import React from "react";
-import { cn } from "@lib/utils";
+"use client";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-const Stops: React.FC<{ className?: string }> = ({ className }) => {
+interface StopProps {
+  initialVpX: number;
+  initialVpY: number;
+  baseHeight: number;
+  baseWidth: number;
+  baseDepth: number;
+}
+
+export default function Stop({
+  initialVpX,
+  initialVpY,
+  baseHeight,
+  baseWidth,
+  baseDepth,
+}: StopProps) {
+  /*   const baseHeight = 13;
+  const baseWidth = 100;
+  const baseDepth = 35; */
+  const buttonHeight = baseHeight;
+  const [buttonClickedHeight, setButtonClickedHeight] = useState(0);
+  const buttonWidth = baseWidth * 0.58;
+  const buttonDepth = baseDepth / 2;
+  const svgHeight = baseHeight + buttonHeight + baseDepth / 2 + buttonDepth;
+
+  const centerBaseEllipse = [baseWidth / 2, buttonHeight + buttonDepth / 2];
+  const centerButtonEllipse = [baseWidth / 2, buttonDepth / 2];
+  const buttonTopLeft = [
+    centerButtonEllipse[0] - buttonWidth / 2,
+    centerButtonEllipse[1],
+  ];
+  const buttonTopRight = [
+    buttonTopLeft[0] + buttonWidth,
+    centerButtonEllipse[1],
+  ];
+  const buttonBottomRight = [
+    buttonTopRight[0],
+    centerButtonEllipse[1] + buttonHeight,
+  ];
+  const buttonBottomLeft = [buttonTopLeft[0], buttonBottomRight[1]];
+  const baseTopLeft = [0, centerBaseEllipse[1]];
+  const baseTopRight = [baseWidth, centerBaseEllipse[1]];
+  const baseBottomRight = [baseTopRight[0], centerBaseEllipse[1] + baseHeight];
+  const baseBottomLeft = [baseTopLeft[0], baseBottomRight[1]];
+
+  const handleClick = () => {
+    setButtonClickedHeight((prev) => (prev === 0 ? buttonHeight * 0.7 : 0));
+  };
+
   return (
+    <svg
+      className="cursor-pointer"
+      width={baseWidth}
+      height={svgHeight}
+      viewBox={`${initialVpX} ${initialVpY} ${baseWidth} ${svgHeight}`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={handleClick}
+    >
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut" /* 
+            repeat: Infinity,
+            repeatType: "loop", */,
+          times: [0, 1.1, 2],
+        }}
+      />
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut" /* 
+            repeat: Infinity,
+            repeatType: "loop", */,
+          delay: 0.25,
+          times: [0, 1.1, 2],
+        }}
+      />
+      <motion.ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1] + baseHeight}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="grey"
+        animate={{
+          scale: [1, 2.5, 1],
+          opacity: [1, 0, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut" /* 
+            repeat: Infinity,
+            repeatType: "loop", */,
+          delay: 0.5,
+          times: [0, 1.1, 2],
+        }}
+      />
+      <path
+        d={`M${baseTopLeft[0]} ${baseTopLeft[1]} L${baseBottomLeft[0]} ${
+          baseBottomLeft[1]
+        } A ${baseWidth / 2} ${baseDepth / 2} 0 1 0 ${baseBottomRight[0]} ${
+          baseBottomRight[1]
+        } L${baseTopRight[0]} ${baseTopRight[1]}`}
+        fill="#4F4F4F"
+        stroke="black"
+      />
+      <ellipse
+        cx={centerBaseEllipse[0]}
+        cy={centerBaseEllipse[1]}
+        rx={baseWidth / 2}
+        ry={baseDepth / 2}
+        fill="#838383"
+        stroke="black"
+      />
+
+      <motion.path
+        d={`M${buttonTopLeft[0]} ${buttonTopLeft[1]} L${buttonBottomLeft[0]} ${
+          buttonBottomLeft[1]
+        } A ${buttonWidth / 2} ${buttonDepth / 2} 0 1 0 ${
+          buttonBottomRight[0]
+        } ${buttonBottomRight[1]} L${buttonTopRight[0]} ${buttonTopRight[1]}`}
+        animate={{
+          d: `M${buttonTopLeft[0]} ${buttonTopLeft[1] + buttonClickedHeight} L${
+            buttonBottomLeft[0]
+          } ${buttonBottomLeft[1]} A ${buttonWidth / 2} ${
+            buttonDepth / 2
+          } 0 1 0 ${buttonBottomRight[0]} ${buttonBottomRight[1]} L${
+            buttonTopRight[0]
+          } ${buttonTopRight[1] + buttonClickedHeight}`,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          damping: 15,
+        }}
+        fill="#4F4F4F"
+        stroke="black"
+      />
+      <motion.ellipse
+        cx={centerButtonEllipse[0]}
+        cy={centerButtonEllipse[1]}
+        rx={buttonWidth / 2}
+        ry={buttonDepth / 2}
+        animate={{
+          cy: centerButtonEllipse[1] + buttonClickedHeight,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          damping: 20,
+        }}
+        fill="#838383"
+        stroke="black"
+      />
+    </svg>
+  );
+}
+
+/* return (
     <div className={cn(className, "flex justify-center items-center")}>
       <div className="absolute w-9 h-9 bg-blue-300 rounded-xl border-2 border-slate-700  z-40"></div>
       <div className="absolute w-9 h-[2.75rem] bg-blue-500 border-2 border-slate-700 rounded-xl z-30 transform translate-y-1"></div>
@@ -12,7 +187,4 @@ const Stops: React.FC<{ className?: string }> = ({ className }) => {
       <div className="absolute w-16 h-16 bg-blue-200 opacity-50 rounded-2xl animate-pingWithDelay delay-300"></div>
       <div className="absolute w-16 h-16 bg-blue-200 opacity-50 rounded-3xl animate-pingWithDelay delay-600"></div>
     </div>
-  );
-};
-
-export default Stops;
+  ); */
